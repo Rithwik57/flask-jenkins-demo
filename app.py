35996ead92862f934c02b350f4e64dc049f6e5c8
Rithwik Mahanti <rithwikmahanti7@gmail.com>
2026-02-12 23:48:37 +0530

    Remove test parameter from hello route response

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     build_id = os.getenv('BUILD_ID', 'Local')
-    return f"<h1>Hello from Flask! (we are now live thru the CI/CD pipeline, test = 2)</h1><p>Build ID: {build_id}</p>"
+    return f"<h1>Hello from Flask! (we are now live thru the CI/CD pipeline)</h1><p>Build ID: {build_id}</p>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
